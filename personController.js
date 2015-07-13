537926cf4b37f1eb5a13946227334d374ee345ea
live4code <live4code@gmail.com>
2015-07-13 18:51:40 +0000

    commit exercise content by Live4Code

---
+++ personController.js
@@ -0,0 +1,11 @@
+angular.module('myApp', [])
+
+.controller('personCtrl', function($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+    
+  $scope.fullName = function() {
+    return $scope.firstName + " " + $scope.lastName;
+  };
+  
+});
